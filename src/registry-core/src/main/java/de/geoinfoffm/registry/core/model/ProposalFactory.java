946b04aa43e12340b13192853b1bf19f770e885d
Florian Esser <fe@bespire.de>
2015-03-30 01:32:19 +0200

    Moved createAddition to ProposalFactory

--- src/registry-core/src/main/java/de/geoinfoffm/registry/core/model/ProposalFactory.java
+++ src/registry-core/src/main/java/de/geoinfoffm/registry/core/model/ProposalFactory.java
@@ -84,4 +84,28 @@ public class ProposalFactory implements Proposal.Factory
 
 		return null;
 	}
+	
+	public Addition createAddition(RE_RegisterItem proposedItem, RE_SubmittingOrganization sponsor, String justification,
+			String registerManagerNotes, String controlBodyNotes) throws InvalidProposalException {
+
+		RE_AdditionInformation additionInformation = new RE_AdditionInformation();
+		// The dateProposed property must not be set here because the proposal
+		// process will not start before the proposal was reviewed by the 
+		// register manager (see sec. 6.2.6.3 of ISO 19135).
+		// Due to technical considerations the proposal management record is created
+		// at this point in time 
+		additionInformation.setSponsor(sponsor);
+		additionInformation.setStatus(RE_DecisionStatus.PENDING);
+		additionInformation.setJustification(justification);
+		additionInformation.setRegisterManagerNotes(registerManagerNotes);
+		additionInformation.setControlBodyNotes(controlBodyNotes);
+		additionInformation.setItem(proposedItem);
+		
+		Addition result = new Addition(additionInformation);
+		
+		workflowManager.initialize(result);
+		
+		return result;
+	}
+
 }
