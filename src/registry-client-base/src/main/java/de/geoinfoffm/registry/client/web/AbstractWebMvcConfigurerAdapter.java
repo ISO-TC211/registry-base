bb2d719f67d50750baafe33b1b74b3c600f59395
Florian Esser <fe@bespire.de>
2015-04-22 17:11:51 +0200

    Updated to thymeleaf-extras-springsecurity4

--- src/registry-client-base/src/main/java/de/geoinfoffm/registry/client/web/AbstractWebMvcConfigurerAdapter.java
+++ src/registry-client-base/src/main/java/de/geoinfoffm/registry/client/web/AbstractWebMvcConfigurerAdapter.java
@@ -67,7 +67,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 import org.springframework.web.servlet.handler.BeanNameUrlHandlerMapping;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-import org.thymeleaf.extras.springsecurity3.dialect.SpringSecurityDialect;
+import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
 import org.thymeleaf.spring3.SpringTemplateEngine;
 import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
