24a57e88992448ccc84530380f00fd7ef445fcfc
Florian Esser <fe@bespire.de>
2015-11-19 15:57:37 +0100

    Removed superfluous import

--- src/registry-api/src/main/java/de/geoinfoffm/registry/api/ProposalTaskManager.java
+++ src/registry-api/src/main/java/de/geoinfoffm/registry/api/ProposalTaskManager.java
@@ -2,7 +2,6 @@ package de.geoinfoffm.registry.api;
 
 import java.util.concurrent.Callable;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.Future;
 import java.util.concurrent.FutureTask;
 
